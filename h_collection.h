e29e0b2e11516f8c595f76dab1e5727cbeb70ddc
gong <generalgong@163.com>
2020-06-18 09:24:01 +0800

    h collection , mutxt

---
+++ h_collection.h
@@ -0,0 +1,51 @@
+// 用于检查返回状态，特别方便
+#define CHECK(call)\
+{\
+  const cudaError_t error=call;\
+  if(error!=cudaSuccess)\
+  {\
+      printf("ERROR: %s:%d,",__FILE__,__LINE__);\
+      printf("code:%d,reason:%s\n",error,cudaGetErrorString(error));\
+      exit(1);\
+  }\
+}
+
+
+// 跨平台的，获取当期时间的函数。 与tic_toc 不同,这个是纯ｃ实现　
+
+#include <time.h>
+#ifdef _WIN32
+#	include <windows.h>
+#else
+#	include <sys/time.h>
+#endif
+#ifdef _WIN32
+int gettimeofday(struct timeval *tp, void *tzp)
+{
+  time_t clock;
+  struct tm tm;
+  SYSTEMTIME wtm;
+  GetLocalTime(&wtm);
+  tm.tm_year   = wtm.wYear - 1900;
+  tm.tm_mon   = wtm.wMonth - 1;
+  tm.tm_mday   = wtm.wDay;
+  tm.tm_hour   = wtm.wHour;
+  tm.tm_min   = wtm.wMinute;
+  tm.tm_sec   = wtm.wSecond;
+  tm. tm_isdst  = -1;
+  clock = mktime(&tm);
+  tp->tv_sec = clock;
+  tp->tv_usec = wtm.wMilliseconds * 1000;
+  return (0);
+}
+#endif
+double cpuSecond()
+{
+  struct timeval tp;
+  gettimeofday(&tp,NULL);
+  return((double)tp.tv_sec+(double)tp.tv_usec*1e-6);
+
+}
+
+
+
